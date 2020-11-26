dd223e4c3342434b1fc999a9490cc55c0b62d2ef
Daniel James Tarplett <danieljamestarplett@hotmail.com>
2020-11-26 16:36:45 +0100

    Added a paddle surface controller and Paddle surface material to split responsibilities from the paddle controller. Started working on the collision between paddle and ball. Still some work that needs done.

---
+++ Assets/Scripts/Physics/PaddleSurfaceController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StupidGirlGames.BreakOut
+{
+    /// <summary>
+    /// Controls the surface of the paddle. A paddle can affect incoming balls depending on
+    /// the collision point on the paddle surface or the paddle speed. By keeping that information
+    /// in this class, we can add different scripts
+    /// 
+    /// </summary>
+    public class PaddleSurfaceController : MonoBehaviour
+    {
+		[Tooltip("The minimum deflection angle from the paddles transform.up")]
+		public float minDeflectionAngle;
+
+		[Tooltip("The maximum deflection angle from the paddles transform.up")]
+		public float maxDeflectionAngle;
+
+		[Tooltip("The Paddle material. If no material is added, the reflections will be perfect.")]
+		public PaddleSurfaceMaterial material;
+
+		private PaddleController PaddleController;
+        private BoxCollider2D paddleCollider;
+
+		private void Awake()
+		{
+			PaddleController = GetComponent<PaddleController>();
+			paddleCollider = GetComponent<BoxCollider2D>();
+		}
+
+		/// <summary>
+		/// This is where the magic happens. The paddle has an affect on the ball depending on the 
+		/// paddle surface material
+		/// </summary>
+		/// <param name="collision"></param>
+		private void OnCollisionEnter2D(Collision2D collision)
+		{
+			BallController ball = collision.gameObject.GetComponent<BallController>();
+			if (ball != null)
+			{
+				Rigidbody2D ballBody = collision.rigidbody;
+				if (ballBody != null)
+				{
+
+					// We check if the collision normal is equal to the paddles transform up.
+					// This is the case where we can do fun stuff with the ball reflections
+					// Note that the == operator for Vector2s and Vector3s is an 
+					// approximatly equal operator.
+					if (collision.GetContact(0).normal == -(Vector2)transform.up)
+					{
+						// We find the position of the top center of the collider. This takes rotation
+						// into consideration
+						Vector2 topMiddle = paddleCollider.bounds.center + transform.up * paddleCollider.size.y / 2;
+
+						// We find the point of collision in world space
+						Vector2 contactPoint = collision.GetContact(0).point;
+
+						// Then the distance between the contact point and the paddle top center
+						Vector2 distance = topMiddle - contactPoint;
+
+						// We find the signed length from the contact point. We must cast to vector3 to be able to find the sign. We use the 
+						// cross product to see if the contact point is to the left or right of the transform up.
+						float signedlength = Mathf.Sign(Vector3.Cross((Vector3)distance, (Vector3)collision.GetContact(0).normal).z)*distance.magnitude;
+
+						// We find the signed velocity along the paddles transform.right axis by using the cross product
+						float signedVelocity = Mathf.Sign(Vector3.Cross((Vector3)collision.otherRigidbody.velocity, transform.up).z) * collision.otherRigidbody.velocity.magnitude;
+						if (material != null)
+						{
+
+							// We calculate the new rotation with the angles added from the contact point and the paddle velocity
+							float posAngle = material.CalculateAngleToAddFromCollision(signedlength, paddleCollider.size.x);
+							float velAngle = material.CalculateAngleToAddFromVelocity(signedVelocity, PaddleController.speed);
+							Quaternion newAngle = Quaternion.AngleAxis(Vector3.SignedAngle(transform.up, collision.rigidbody.position, transform.forward) + posAngle + velAngle, Vector3.forward);
+							ball.SetDirection(newAngle * transform.up);
+						}
+
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Physics/PaddleSurfaceMaterial.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A paddle surface material holds information about the surface characteristics of a paddle.
+/// When the ball hits the paddle the distance from the center has an affect on the outgoing 
+/// angle. The paddle speed can also have an affect, and these characteristics are in this class
+/// </summary>
+[CreateAssetMenu(menuName = "CustomPhysics/PaddleSurfaceMaterial")]
+public class PaddleSurfaceMaterial : ScriptableObject
+{
+	[Tooltip("The maximum added angle from collision with the paddle. The added angle will never go beyond this")]
+	public float maximumAngleToAddFromCollision;
+
+	[Tooltip("The sweetspot of the paddle. When the ball hits a position within the sweet spot, no angle is added")]
+	public float sweetSpotDistance;
+
+	[Tooltip("The maximum added angle from the paddle velocity")]
+	public float maximumAngleToAddFromVelocity;
+
+	[Tooltip("The minimum velocity needed to add an angle from velocity")]
+	public float minimumVelocity;
+
+	/// <summary>
+	/// Calculates an angle to add to a reflection of a ball from the paddle. The angle added is dependent on the position from the center of the 
+	/// paddle. The angle never exceeds the maximumangleToAdd This method uses a linear model.
+	/// </summary>
+	/// <param name="positionFromCenter"></param>
+	/// <param name="width"></param>
+	/// <returns></returns>
+	public float CalculateAngleToAddFromCollision(float positionFromCenter, float width)
+	{
+		return positionFromCenter < sweetSpotDistance ? 0 : maximumAngleToAddFromCollision*positionFromCenter/width;
+	}
+
+	/// <summary>
+	/// Calculates an angle to add from the velocity of the paddle. The speed of the paddle must be larger than the minimumVelocity to
+	/// take affect. The angle also never exceeds the maximumAngleToAddFromVelocity.
+	/// </summary>
+	/// <param name="velocity"></param>
+	/// <param name="maxVelocity"></param>
+	/// <returns></returns>
+	public float CalculateAngleToAddFromVelocity(float velocity, float maxVelocity)
+	{
+		return velocity < minimumVelocity ? 0 : (velocity < maxVelocity ? maximumAngleToAddFromVelocity * velocity / maxVelocity : maxVelocity);
+	}
+}
